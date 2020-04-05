98184a5481e6cb70f9872a047cedde27ff00a0b5
ANGEL778979 <r4105224749@sina.com>
2020-04-05 17:06:04 +0800

    first commit

---
+++ 11.cpp
@@ -0,0 +1,89 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+#include<string.h>
+#define maxsize 100
+
+typedef char datatype;
+
+typedef struct nodeS{
+	datatype el;
+	nodeS *next;
+}NodeS;
+
+NodeS *top;
+
+//÷√ø’’ª
+NodeS *SetnullS2(NodeS *S)
+{
+	S->next=NULL;
+	return S;
+ } 
+//≈–∂œ’ª «∑ÒŒ™ø’
+int IsemptyS2(NodeS *S)
+{
+	if(S->next==NULL)
+		return 0;
+	else return 1;
+ } 
+ 
+//Ω¯’ª
+NodeS *PushS2(NodeS *S,datatype e)
+{
+	NodeS *p;
+	p=(NodeS*)malloc(sizeof(NodeS));
+	p->el=e;
+	p->next=S;
+	S=p;
+	return S;
+		
+}
+//≥ˆ’ª£¨…æ≥˝’ªSµƒ’ª∂•‘™Àÿ 
+void PopS2(NodeS *S)
+{
+	if(S==NULL)
+		printf("stack is underflow\n");
+	else
+		S=S->next;
+} 
+
+//11Ã‚ 
+int Issym(char a[],NodeS *S)
+{	
+	int len,i;
+	
+	len=strlen(a);
+	
+	for(i=0;i<(len/2);i++)
+	  PushS2(S,a[i]);
+	
+	for(;i<len;i++)
+	  {if(S->el==a[i])
+	    PopS2(S);
+	   else 
+	   	return 0;
+		}
+	return 1; 
+}
+
+int main()
+{
+	int i=0,var;
+	char a[maxsize],ch;
+	NodeS *S;
+	
+	scanf("%s",a);
+	printf("123\n");
+	
+	S->next=NULL;
+	var=Issym(a,S);
+	printf("789\n");
+	if(var)
+	 printf("str is symmerty\n");
+	else printf("str is not symmerty\n");
+	printf("helloworld"); 
+	return 0;
+	
+	
+	
+}
---
+++ 12.cpp
@@ -0,0 +1,89 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+#include<string.h>
+#define maxsize 100
+
+typedef char datatype;
+
+typedef struct nodeS{
+	datatype el;
+	nodeS *next;
+}NodeS;
+
+//判断栈是否为空
+int IsemptyS2(NodeS *S)
+{
+	if(S->next==NULL)
+		return 1;
+	else return 0;
+ } 
+ 
+//进栈
+NodeS *PushS2(NodeS *S,datatype e)
+{
+	NodeS *p;
+	p=(NodeS*)malloc(sizeof(NodeS));
+	p->el=e;
+	p->next=S;
+	S=p;
+	return S;
+		
+}
+//出栈，删除栈S的栈顶元素，并返回栈顶的数据元素。
+datatype *PopS2(NodeS *S)
+{
+	datatype *ret;
+	if(S==NULL)
+	{	printf("stack is underflow\n");
+	}
+	
+	else
+	{
+		ret=(datatype*)malloc(sizeof(datatype));
+		*ret=S->el;
+		S=S->next;
+		return ret;
+	}
+} 
+
+
+
+int cp(char a[])
+{
+	int i;
+	char *ret;
+	NodeS *s1,*s2;
+	s1->next=NULL;s2->next=NULL;
+	
+	if(a[0]!=')')
+	  for(i=0;i<strlen(a);i++)
+	    {if(a[i]=='(')
+	       PushS2(s1,a[i]);
+	     else if(a[i]==')')
+		   ret=PopS2(s1);
+		 else  PushS2(s2,a[i]);
+	    }
+	else return 0;
+	
+	if(IsemptyS2(s1))
+	  return 1;
+	else return 0;
+	
+	
+}
+
+int main()
+{
+	int n,i;
+	scanf("%d",&n);
+	char a[n];
+	for(i=0;i<n;i++)
+	  scanf("%d",&a[i]);
+	
+	if(cp(a))
+	 printf("yes\n");
+	else printf("no\n");
+	
+	return 0;
+}
